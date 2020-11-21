306e98eb88cb25a8bfdcf7f57026f29a1214fe28
Giulliano Paz <giullianopaz.aluno@unipampa.edu.br>
2020-11-21 13:38:03 -0300

    feat: Company model definition

--- apps/companies/admin.py
+++ apps/companies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.companies.models import Company
+
+
+admin.site.register(Company)
--- apps/companies/models.py
+++ apps/companies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(verbose_name='Razão Social', max_length=200, unique=True)
+    trading_name = models.CharField(verbose_name='Nome Fantasia', max_length=200, blank=True, null=True)
+    registered_number = models.CharField(verbose_name='CNPJ', max_length=20, unique=True)
+    email = models.EmailField(verbose_name='Email', max_length=100, unique=True)
+    phone = models.CharField(verbose_name='Telefone', max_length=20, blank=True, null=True)
+
+    employees = models.ManyToManyField('users.User', verbose_name='Funcionários')
+
+    class Meta:
+        ordering = ['name', 'trading_name']
+
+    def __str__(self):
+        return self.name
