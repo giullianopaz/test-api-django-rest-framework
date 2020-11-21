99f513e67afde1394c982585c0b710b8d6fca25f
Giulliano Paz <giullianopaz.aluno@unipampa.edu.br>
2020-11-21 17:24:19 -0300

    feat: User API tests creation

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -15,3 +15,10 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        super().update(instance, validated_data)
+        password = validated_data.pop('password')
+        instance.set_password(password)
+        instance.save()
+        return instance
