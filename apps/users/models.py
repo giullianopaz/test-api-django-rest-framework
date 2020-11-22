6de217f9ced350046cc085a9e7e15f2ea2ed3315
Giulliano Paz <giullianopaz.aluno@unipampa.edu.br>
2020-11-22 14:16:04 -0300

    refac: Changing the way of manipulate users and companies relations

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 
+from apps.companies.models import Company, Membership
+
 
 class User(AbstractUser):
     """
@@ -14,3 +16,45 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.get_full_name()
+
+    def add_companies(self, companies):
+        if companies is None:
+            return
+        companies = [companies] if not isinstance(companies, (list, tuple)) else companies
+        for company_pk in companies:
+            if Company.objects.filter(pk=company_pk).exists():
+                company_instance = Company.objects.get(pk=company_pk)
+                if not Membership.objects.filter(company=company_instance, user=self).exists():
+                    Membership.objects.create(
+                        company=company_instance,
+                        user=self
+                    )
+
+    def get_companies(self):
+        return self.companies.all()
+
+    def update_companies(self, companies):
+        if companies is None:
+            return
+        self.companies.clear()
+        companies = [companies] if not isinstance(companies, (list, tuple)) else companies
+        for company_pk in companies:
+            if Company.objects.filter(pk=company_pk).exists():
+                company_instance = Company.objects.get(pk=company_pk)
+                if not Membership.objects.filter(company=company_instance, user=self).exists():
+                    Membership.objects.create(
+                        company=company_instance,
+                        user=self
+                    )
+
+    def delete_companies(self, companies):
+        if companies is None:
+            return
+        companies = [companies] if not isinstance(companies, (list, tuple)) else companies
+        for company_pk in companies:
+            if Company.objects.filter(pk=company_pk).exists():
+                company_instance = Company.objects.get(pk=company_pk)
+                company_instance.employees.remove(self)
+
+    def clean_companies(self):
+        self.companies.clear()
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from apps.users.models import User
 from apps.users.serializers import UserSerializer
+from apps.companies.serializers import CompanySerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,7 +14,33 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     lookup_field = 'username'
 
-    def get_serializer_class(self):
-        self.serializer_class = super().get_serializer_class()
-        self.serializer_class.Meta.depth = 1 if 'related' in self.request.query_params else None
-        return self.serializer_class
+    @action(detail=True, methods=['post'])
+    def add_companies(self, request, *args, **kwargs):
+        user = self.get_object()
+        if 'companies' in request.data:
+            user.add_companies(request.data.get('companies'))
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['put'])
+    def update_companies(self, request, *args, **kwargs):
+        user = self.get_object()
+        if 'companies' in request.data:
+            user.update_companies(request.data.get('companies'))
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['delete'])
+    def delete_companies(self, request, *args, **kwargs):
+        user = self.get_object()
+        if 'companies' in request.data:
+            user.delete_companies(request.data.get('companies'))
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['delete'])
+    def clean_companies(self, request, *args, **kwargs):
+        user = self.get_object()
+        user.clean_companies()
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
