2b387bfdf758664e34cc25dbfb8a26df3bc316f0
Giulliano Paz <giullianopaz.aluno@unipampa.edu.br>
2020-11-22 00:39:13 -0300

    feat: Choose whether to return relations

--- apps/users/views.py
+++ apps/users/views.py
@@ -10,3 +10,8 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     lookup_field = 'username'
+
+    def get_serializer_class(self):
+        self.serializer_class = super().get_serializer_class()
+        self.serializer_class.Meta.depth = 1 if 'related' in self.request.query_params else None
+        return self.serializer_class
