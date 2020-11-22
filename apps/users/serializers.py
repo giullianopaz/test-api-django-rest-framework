5c5ac0db5eb4b7791d65f50cd6b50da640a81ad4
Giulliano Paz <giullianopaz.aluno@unipampa.edu.br>
2020-11-21 23:35:40 -0300

    fix: Checking password in the update method

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = User(**validated_data)
-        if password or password != '':
+        if password and password != '':
             user.set_password(password)
         user.save()
         return user
@@ -23,6 +23,7 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         super().update(instance, validated_data)
         password = validated_data.pop('password')
-        instance.set_password(password)
+        if password and password != '':
+            instance.set_password(password)
         instance.save()
         return instance
